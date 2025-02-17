a99eeacf512e0c5e66a5e599ec2e5db36b9dd1df
Neel Kant <neel@hippocraticai.com>
2025-02-17 17:28:08 +0000

    fix removal of old node info files

--- visualize_inference_cluster.sh
+++ visualize_inference_cluster.sh
@@ -1,12 +1,13 @@
 # First create the inventory file from running the python script
 source ~/venv/bin/activate
-python /fsx/ubuntu/matt/cluster_visualization/create_inventory_file.py --partition inference
+cd /fsx/ubuntu/matt/cluster_visualization
+python create_inventory_file.py --partition inference
 
 # Then run the ansible playbook with the inventory file,
 # passing in a base directory for CSV files (update the basedir as needed)
-ansible-playbook -i /fsx/ubuntu/matt/cluster_visualization/inventory_ml.inference.ini \
-  /fsx/ubuntu/matt/cluster_visualization/gpu_info_ansible.yaml \
+ansible-playbook -i inventory_ml.inference.ini \
+  gpu_info_ansible.yaml \
   -f 100 --extra-vars "basedir=/fsx/ubuntu/matt/cluster_visualization"
 
 # Then run the python script to create the html file
-python /fsx/ubuntu/matt/cluster_visualization/generate_gpu_viz.py
+python generate_gpu_viz.py
